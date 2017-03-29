67aeed46b30a12f0fe154a9c2aa0fafe4c9d81d2
AquariusPower <AquariusPower@users.noreply.github.com>
2017-03-29 19:25:23 -0300

    ResizablePanel WIP

---
+++ src/main/java/com/simsilica/lemur/ResizablePanel.java
@@ -0,0 +1,83 @@
+package com.github.devconslejme;
+
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.simsilica.lemur.GuiGlobals;
+import com.simsilica.lemur.Panel;
+import com.simsilica.lemur.component.BorderLayout;
+import com.simsilica.lemur.component.QuadBackgroundComponent;
+import com.simsilica.lemur.core.GuiComponent;
+import com.simsilica.lemur.core.GuiControl;
+import com.simsilica.lemur.style.Attributes;
+import com.simsilica.lemur.style.ElementId;
+import com.simsilica.lemur.style.StyleAttribute;
+import com.simsilica.lemur.style.StyleDefaults;
+import com.simsilica.lemur.style.Styles;
+
+public class ResizablePanel extends Panel {
+	private BorderLayout layout;
+	private Panel contents;
+	private int iBorderSize = 3;
+	
+	public static final String LAYER_RESIZABLE_BORDERS = "resizableBorders";
+	
+  public ResizablePanel( float width, float height, String style ) {
+    super(false, new ElementId("resizablePanel"), style);
+    
+    this.layout = new BorderLayout();
+    getControl(GuiControl.class).setLayout(layout);
+    
+    getControl(GuiControl.class).setPreferredSize(new Vector3f(width, height, 0));
+    
+    // Set our layers
+    getControl(GuiControl.class).setLayerOrder(LAYER_INSETS, 
+                                               LAYER_BORDER, 
+                                               LAYER_BACKGROUND,
+                                               LAYER_RESIZABLE_BORDERS);
+    
+    Styles styles = GuiGlobals.getInstance().getStyles();
+    styles.applyStyles(this, getElementId().getId(), style);
+  }
+	
+  @StyleDefaults("resizablePanel")
+  public static void initializeDefaultStyles( Attributes attrs ) {
+      attrs.set( "resizableBorders", new QuadBackgroundComponent(ColorRGBA.Gray), false );
+  }
+  
+	@StyleAttribute(value="resizableBorders", lookupDefault=false)
+	public void setResizableBorders( GuiComponent bg ) {        
+	    getControl(GuiControl.class).setComponent(LAYER_RESIZABLE_BORDERS, bg);   
+	}
+	
+  public GuiComponent getResizableBorders() {
+    return getControl(GuiControl.class).getComponent(LAYER_RESIZABLE_BORDERS);
+  }
+  
+	/**
+	 *  Resets the child contents that will be expanded/collapsed
+	 *  with the rollup.
+	 */
+	public void setContents( Panel p ) {
+	    if( this.contents == p ) {
+	        return;
+	    }
+	    
+	    if( this.contents != null) {
+	        layout.removeChild(contents);
+	    }
+	    
+	    this.contents = p;
+	    if( this.contents != null ) {
+	      if( contents.getParent() == null ) {
+	        layout.addChild(contents,  BorderLayout.Position.Center);
+	        setBorderSize(iBorderSize); //prepare the new one
+	      }
+	    }
+	}
+	
+	public ResizablePanel setBorderSize(int i){
+		this.iBorderSize=i;
+    contents.setPreferredSize(getPreferredSize().subtract(new Vector3f(iBorderSize*2,iBorderSize*2,0)));
+		return this;
+	}
+}
